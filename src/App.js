e740c87bd10dcb41ae9905efd4c015444ec4b188
ShafferMichael <shaffer.michael@ufl.edu>
2022-08-02 16:26:20 -0400

    googel auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./App.css";
+import { auths } from "./firebase-config";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
-  return <div>App</div>;
-}
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auths, provider)
+      .then((re) => {
+        console.log(re);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  return (
+    <div className="App">
+      <button onClick={signInWithGoogle}>Click Me</button>
+    </div>
+  );
+}
 export default App;
