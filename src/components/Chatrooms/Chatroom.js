d51fd8203a9b4fc2b6443999d75ad07ca3485e93
Zadys <59520611+Zadys@users.noreply.github.com>
2022-08-04 14:36:17 -0400

    Bootstrap styles

--- src/components/Chatrooms/Chatroom.js
+++ src/components/Chatrooms/Chatroom.js
@@ -10,21 +10,83 @@ function Chatroom(){
     return (
         <div className="Chat">
             <Header />
-            <button
-              onClick ={() => {
-                navigate("/");
-                }}> Go back to Home 
+            
+            <div class="container">
+            <div class="col-md-4">
+            <button class="btn btn-primary"
+                onClick ={() => {
+                    navigate("/");
+                    }}> Go back to Home 
             </button>
-            <hr />
-            <div>Select the chat room you wish to join below!</div>
-            <hr />
-            <button onClick ={() => {navigate("/generalchat")}}>General Chat</button>
-            <button onClick ={() => {navigate("/soccerchat")}}>Soccer Chat</button>
-            <button onClick ={() => {navigate("/chemistrychat")}}>Chemistry Chat</button>
-            <hr />
-            <button onClick ={() => {navigate("/videogameschat")}}>Video Games Chat</button>
-            <button onClick ={() => {navigate("/photographychat")}}>Photography Chat</button>
-            <button onClick ={() => {navigate("/hikingchat")}}>Hiking Chat</button>
+            </div>
+            
+    
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top chat-img" src="general_chat.png" ></img>
+                            <div class="card-body">
+                                <h5 class="card-title">General Chat</h5>
+                                <p class="card-text">A room where all UF students can come together and socialze.</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/generalchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="card-img-top chat-img" src="soccer_chat.jpg" ></img>
+                            <div class="card-body">
+                                <h5>Soccer Chat</h5>
+                                <p>A room where soccer fans to discuss the sport and find others to play with.</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/soccerchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="card-img-top chat-img" src="chemistry_chat.jpg" ></img>
+                            <div class="card-body">
+                                <h5>Chemistry Chat</h5>
+                                <p>Come on in and discuss chemistry with us, maybe find your next study partner!</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/chemistrychat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="card-img-top chat-img" src="video_games_chat.png" ></img>
+                            <div class="card-body">
+                                <h5>Video Games Chat</h5>
+                                <p>A room for gamers of all backgrounds to discuss and find others to play with.</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/videogameschat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="card-img-top chat-img" src="photography_chat.ico" ></img>
+                            <div class="card-body">
+                                <h5>Photography Chat</h5>
+                                <p>A place where photography fans can share their pictures and discuss.</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/photographychat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="card-img-top chat-img" src="hiking_chat.png" ></img>
+                            <div class="card-body">
+                                <h5>Hiking Chat</h5>
+                                <p>Come find the hottest hiking trails and make some friends to go with!</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/hikingchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
