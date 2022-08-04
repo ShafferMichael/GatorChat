12b87390b93e039ed20e38f2a3dc07414f626c8c
Zadys <59520611+Zadys@users.noreply.github.com>
2022-08-04 03:13:38 -0400

    Added styling to landing and chat room page

--- src/Home.js
+++ src/Home.js
@@ -19,19 +19,40 @@ function Home() {
   };
 
   return (
-    <div className="App">
-      <Header />
-      <button 
-      onClick={signInWithGoogle}>Sign In
-      </button>
-      <hr />
-      <button 
-              onClick ={() => {
+    <body id="page">
+    <Header/>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6">
+          <form id="login">
+            <div class="form-group">
+              <input type="email" class="form-control" placeholder="UF email"></input>
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" placeholder="Password"></input>
+            </div>
+            <div class="d-grid gap-2">
+              <button type="button" class="btn btn-primary" onClick={signInWithGoogle}>Log in</button>
+              <button type="button" class="btn btn-primary"
+                onClick ={() => {
                 navigate("/chatroom");
                 }}> Enter Chatroom
-        </button>
-    </div>
+              </button>
+            </div>
+          
+          <p class="fs-6
+          ">Don't have an account? <a href="#!" onClick={signInWithGoogle} class="link-info">Register here</a></p>
+          </form>
+        </div>
 
+        <div class="col-sm-6">
+          <img src="landing-image.jpg" alt="img" width="500" height="500"></img>
+        </div>
+      </div>
+
+      </div>
+    </body>
+    
   );
 }
 export default Home;
--- src/components/Chatrooms/Chatroom.js
+++ src/components/Chatrooms/Chatroom.js
@@ -10,21 +10,83 @@ function Chatroom(){
     return (
         <div className="Chat">
             <Header />
-            <button
-              onClick ={() => {
-                navigate("/");
-                }}> Go back to Home 
+            
+            <div class="container">
+            <div class="col-md-4">
+            <button class="btn btn-primary"
+                onClick ={() => {
+                    navigate("/");
+                    }}> Go back to Home 
             </button>
-            <hr />
-            <div>Select the chat room you wish to join below!</div>
-            <hr />
-            <button onClick ={() => {navigate("/generalchat")}}>General Chat</button>
-            <button onClick ={() => {navigate("/soccerchat")}}>Soccer Chat</button>
-            <button onClick ={() => {navigate("/chemistrychat")}}>Chemistry Chat</button>
-            <hr />
-            <button onClick ={() => {navigate("/videogameschat")}}>Video Games Chat</button>
-            <button onClick ={() => {navigate("/photographychat")}}>Photography Chat</button>
-            <button onClick ={() => {navigate("/hikingchat")}}>Hiking Chat</button>
+            </div>
+            
+    
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="chat-img" src="general_chat.png" ></img>
+                            <div class="card-body">
+                                <h5>General Chat</h5>
+                                <p>General chat description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/generalchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="chat-img" src="soccer_chat.jpg" ></img>
+                            <div class="card-body">
+                                <h5>Soccer Chat</h5>
+                                <p>Soccer chat description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/soccerchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="chat-img" src="chemistry_chat.jpg" ></img>
+                            <div class="card-body">
+                                <h5>Chemistry Chat</h5>
+                                <p>Chemistry chat description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/chemistrychat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <img class="chat-img" src="video_games_chat.png" ></img>
+                            <div class="card-body">
+                                <h5>Video Games Chat</h5>
+                                <p>Video Games chat description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/videogameschat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="chat-img" src="photography_chat.ico" ></img>
+                            <div class="card-body">
+                                <h5>Photography Chat</h5>
+                                <p>Photography description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/photographychat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card">
+                            <img class="chat-img" src="hiking_chat.png" ></img>
+                            <div class="card-body">
+                                <h5>Hiking Chat</h5>
+                                <p>Hiking chat description goes here</p>
+                                <button class="btn btn-primary" onClick ={() => {navigate("/hikingchat")}}>Join chatroom</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
